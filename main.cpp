7f7c0553a820a803e27388a55af88565222c6028
ellisrourkePC <ellisrourke@gmail.com>
2020-03-18 19:18:51 +1000

    DONE

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
-int countWays(std::vector<int> &coins,int index, int n, int lowerLimit,int upperLimit,int recursiveDepth){
+int countWays(std::vector<int> &coins,int index, int n, int lowerLimit,int upperLimit,int recursiveDepth,int count){
     if(n == 0){
-        if((recursiveDepth + coins[index-1]) <= 0 && recursiveDepth + lowerLimit >= index-1){
+        std::cout << "steps = " << (count) << std::endl;
+        //if((recursiveDepth + coins[index-1])>=0){
+        if(count <= upperLimit && count >= lowerLimit){
             return 1;
         } else
             return 0;
@@ -11,16 +13,18 @@ int countWays(std::vector<int> &coins,int index, int n, int lowerLimit,int upper
         return 0;
     }
     else if(index == coins.size() && n > 0){
+
         return 0;
     }
-    return countWays(coins, index, n - coins[index],lowerLimit,upperLimit,recursiveDepth-1) + countWays(coins, index + 1, n,lowerLimit,upperLimit,recursiveDepth-1);
 
+    return countWays(coins, index, n - coins[index],lowerLimit,upperLimit,recursiveDepth-1,count+1) + countWays(coins, index + 1, n,lowerLimit,upperLimit,recursiveDepth-1,count);
 }
 int main() {
-    std::vector<int> coins = {1,2,3,5,7};
+    std::vector<int> coins = {1,2,3,5};
     int index = 0;
-    int n = 8;
+    int n = 6;
     int lowerLimit = 5;
-    int upperLimit = 5;
-    std::cout << countWays(coins,index,n,lowerLimit,upperLimit,upperLimit) << " ways to make change" << std::endl;
+    int upperLimit = 6;
+    int count = 0;
+    std::cout << countWays(coins,index,n,lowerLimit,upperLimit,upperLimit,count) << " ways to make change" << std::endl;
 }
